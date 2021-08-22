257429a79e641a562971b9aab96ea99696b6dec8
Davidhct <davidhct1@gmail.com>
2021-08-22 13:42:18 +0300

    create solver

--- index.js
+++ index.js
@@ -20,6 +20,17 @@ let grid1 = [
   [7, 4, 6, 3, 2, 5, 8, 1, 9],
   [3, 2, 8, 1, 9, 6, 5, 4, 7],
 ];
+let grid3 = [
+  [8, 9, 5, 7, 4, 2, 1, 3, 6],
+  [2, 7, 1, 9, 6, 3, 4, 8, 5],
+  [4, 6, 3, 5, 8, 1, 7, 9, 2],
+  [9, 3, 4, 6, 1, 7, 2, 5, 8],
+  [5, 1, 7, 2, 3, 8, 9, 6, 4],
+  [6, 8, 2, 4, 5, 9, 3, 7, 1],
+  [1, 5, 9, 8, 7, 4, 6, 2, 3],
+  [7, 4, 6, 3, 2, 5, 8, 1, 9],
+  [3, 2, 8, 1, 9, 6, 5, 4, 7],
+];
 const globalVar = {
   grid: grid2,
   nums: [1, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -33,7 +44,6 @@ const globalVar = {
   cellCount: true,
   totalNumCell: 81,
   noteFlag: false,
-  flag: true,
 };
 
 const numbersBtn = document.getElementsByClassName("number");
@@ -42,6 +52,7 @@ const sudokuTable = document.querySelector("table");
 const undoBtn = document.getElementById("undo-btn");
 const eraseBtn = document.getElementById("erase-btn");
 const notesBtn = document.getElementById("notes-btn");
+const solveBtn = document.getElementById("solveBtn");
 // const hintBtn = document.getElementById("hint-btn");
 
 class Sudoku {
@@ -192,11 +203,12 @@ class Sudoku {
 
   checkGrid() {
     let flagValid;
-    num = this.nums;
+    const num = this.nums;
 
     for (let i = 0; i < this.grid.length; i++) {
       for (let j = 0; j < this.grid.length; j++) {
-        flagValid = sudoku.isValidSudoku(j, j, num[i]);
+        // console.log(this.grid.length);
+        flagValid = sudoku.isValidSudoku(j, j, num[i], true);
         if (flagValid === false) {
           console.log("flagValid = ", flagValid);
           return false;
@@ -209,21 +221,28 @@ class Sudoku {
   }
 
   // Check row
-  isValidSudoku(row, col, num) {
+  isValidSudoku(row, col, num, flag) {
     const rowMap = new Map();
     for (let i = 0; i < this.grid.length; i++) {
-      if (this.grid[row][i] === num) {
-        if (rowMap.get(this.grid[row][i]) === 0) return false;
-        rowMap.set(this.grid[row][i], 0);
+      // console.log(this.grid[0][0]);
+      if (this.grid[row][i] === num && i !== col) {
+        if (rowMap.get(num) === true && flag) {
+          return false;
+        }
+        flag ? rowMap.set(num, true) : null;
+        if (!flag) return false;
       }
     }
 
     // Check col
     const colMap = new Map();
     for (let i = 0; i < this.grid.length; i++) {
-      if (this.grid[i][col] === num) {
-        if (colMap.get(this.grid[i][col]) === 0) return false;
-        colMap.set(this.grid[i][col], 0);
+      if (this.grid[i][col] === num && i !== row) {
+        if (colMap.get(num) === true && flag) {
+          return false;
+        }
+        flag ? colMap.set(this.grid[i][col], true) : null;
+        if (!flag) return false;
       }
     }
 
@@ -236,9 +255,12 @@ class Sudoku {
     for (let i = rowCorner; i < rowCorner + squareSize; i++) {
       // Iterate through each column
       for (let j = colCorner; j < colCorner + squareSize; j++) {
-        if (this.grid[i][j] === num) {
-          if (squareMap.get(this.grid[i][j]) === 0) return false;
-          squareMap.set(this.grid[i][j], 0);
+        if (this.grid[i][j] === num && i !== row && j !== col) {
+          if (squareMap.get(this.grid[i][j]) === true && flag) {
+            return false;
+          }
+          flag ? squareMap.set(this.grid[i][j], true) : null;
+          if (!flag) return false;
         }
       }
     }
@@ -314,69 +336,57 @@ class Sudoku {
       });
     }
   }
-}
+  findEmptysquare() {
+    for (let i = 0; i < this.grid.length; i++) {
+      for (let j = 0; j < this.grid.length; j++) {
+        if (this.grid[i][j] === 0) return [i, j];
+      }
+    }
+    checkBtn.disabled = false;
+    return false;
+  }
 
+  solver() {
+    let row, col;
+
+    const find = sudoku.findEmptysquare();
+    if (find === false) {
+      // checkBtn.disabled = false;
+      return true;
+    } else [row, col] = [...find];
+    // console.log(this.grid);
+
+    for (let i = 1; i < 10; i++) {
+      // console.log(sudoku.isValidSudoku(row, col, i));
+      if (sudoku.isValidSudoku(row, col, i, false)) {
+        // console.log(row, col);
+        this.grid[row][col] = i;
+
+        if (sudoku.solver()) {
+          // console.log(count++);
+          sudoku.displayGrid();
+          return true;
+        }
+
+        this.grid[row][col] = 0;
+      }
+    }
+    return false;
+  }
+}
+let count = 0;
 const sudoku = new Sudoku();
 sudoku.isComplete();
-sudoku.displayGrid();
+
 sudoku.listenerButtonNumber();
 sudokuTable.addEventListener("click", sudoku.enterNumber);
 checkBtn.addEventListener("click", sudoku.checkGrid.bind(sudoku));
 undoBtn.addEventListener("click", sudoku.undo.bind(sudoku));
 eraseBtn.addEventListener("click", sudoku.erase);
 notesBtn.addEventListener("click", sudoku.notes);
+solveBtn.addEventListener("click", sudoku.solver.bind(sudoku));
+sudoku.displayGrid();
 
-// document
-//   .getElementById("check-btn")
-//   .addEventListener("click", sudoku.checkComplition.bind(sudoku));
-
-// validation(cell, num) {
-//   // Check row
-//   let count = 0;
-//   for (let i = 0; i < this.grid.length; i++) {
-//     if (
-//       Number(document.getElementById(`${cell[0]}${i}`).innerHTML) ===
-//       Number(num)
-//     ) {
-//       count++;
-//       if (count > 1) {
-//         console.log(`${cell[0]}${i}`, "1");
-//         return false;
-//       }
-//     }
-//   }
-//   count = 0;
-//   // Check col
-//   for (let i = 0; i < this.grid.length; i++) {
-//     if (
-//       Number(document.getElementById(`${i}${cell[1]}`).innerHTML) ===
-//       Number(num)
-//     ) {
-//       count++;
-//       if (count > 1) {
-//         console.log(`${cell[0]}${i}`, "2");
-//         return false;
-//       }
-//     }
-//   }
-//   count = 0;
-//   // Check box
-//   let row = Math.floor(cell[0] / 3) * 3;
-//   let col = Math.floor(cell[1] / 3) * 3;
-
-//   for (let i = row; i <= 2 + row; i++) {
-//     for (let j = col; i <= 2 + col; j++) {
-//       console.log(document.getElementById(`${i}${j}`).innerHTML);
-//       if (
-//         Number(document.getElementById(`${i}${j}`).innerHTML) === Number(num)
-//       ) {
-//         count++;
-//         if (count > 1) {
-//           console.log(`${cell[0]}${i}`, "3");
-//           return false;
-//         }
-//       }
-//     }
-//   }
-//   return true;
-// }
+onload = () => {
+  if (sudoku.findEmptysquare() === false) checkBtn.disabled = false;
+};
